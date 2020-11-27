0c35d52c1edef2f95e06fbaa6635b35bfe379e48
Edi <60546060+Edi10-developer@users.noreply.github.com>
2020-11-27 18:49:36 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+// Email Submit
+// Note: filter_var() requires PHP >= 5.2.0
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+ 
+  // detect & prevent header injections
+  $test = "/(content-type|bcc:|cc:|to:)/i";
+  foreach ( $_POST as $key => $val ) {
+    if ( preg_match( $test, $val ) ) {
+      exit;
+    }
+  }
+
+$password = "password";
+$headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
+    'Reply-To: ' . $_POST["email"] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  //
+ 
+  $mail_status = mail( "eddy_staff@hotmail.it", $_POST['subject'], $_POST['message'], $headers );
+ 
+  //      ^
+  //  Replace with your email 
+ 
+
+  if ($mail_status) { ?>
+    <script language="javascript" type="text/javascript" >
+      alert('Thank you for the message. We will contact you shortly.');
+      window.location = 'index.html';
+    </script>
+    <?php
+  }
+  else { ?>
+      <script language="javascript" type="text/javascript">
+        alert('Message failed. Please, send an email to ronildabeautycorner@gmail.com');
+        window.location = 'index.html';
+      </script>
+      <?php
+}
+header("Location: ./index.html");
+
+}
+?>
